2e3a2f3a925f590cafa90d0bc7766ff615f5176d
wewe231223 <wewe231223@kpu.ac.kr>
2023-09-27 17:47:19 +0900

    직선 그리기 완성

--- line.py
+++ line.py
@@ -53,8 +53,18 @@ def draw_point(p):
 def draw_line(p1, p2):
     draw_big_point(p1)
     draw_big_point(p2)
+    x1,y1 = p1[0], p1[1] # -100,-100
+    x2,y2 = p2[0], p2[1] # 300,150
+
+    a = (y2 - y1) / (x2 - x1)
+
+    b = y1 - a * x1
+
+    for x in range(x1,x2,10):
+        y = a * x + b
+        draw_point((x,y))
+
     
-    pass
 
 
 prepare_turtle_canvas()
